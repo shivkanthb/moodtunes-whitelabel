9243108f7d82bb352a1a0a0e75961d949f07943c
shivkanthb <shivkanthb@gmail.com>
2016-12-01 22:05:46 -0700

    white label template

--- app.js
+++ app.js
@@ -1,29 +1,74 @@
+
+
 var express = require('express');
-var cons = require('consolidate');
 var path = require('path');
-var app = express();
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var port = process.env.PORT || 3000;
-var fs = require('fs');
+var swig = require('swig');
 
-app.use(express.static(__dirname));
+var routes = require('./routes/index');
+var users = require('./routes/user');
 
-app.engine('html', cons.swig);
-app.set('views', __dirname + '/views');
+var app = express();
+
+var env = process.env.NODE_ENV || 'development';
+app.locals.ENV = env;
+app.locals.ENV_DEVELOPMENT = env == 'development';
+
+// view engine setup
+app.engine('html', swig.renderFile)
+app.set('view cache', false);
+swig.setDefaults({ cache: false });
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
+// app.use(favicon(__dirname + '/public/img/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// var experience = [
-// 	{"company":"university of arizona","role":"teaching assistant","desc":"TA for CSc425 computer networks"},
-// 	{"company":"Recruiting ventures","role":"Software engineering Intern","desc":"Worked with the backend team"}
-// 	];
+app.use('/', routes);
+app.use('/users', users);
 
-app.get('/', function(req,res){
-	 		res.render('index');
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-app.listen(port);
-console.log("Server running on port"+port);
+/// error handlers
+
+// development error handler
+// will print stacktrace
+
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err,
+            title: 'error'
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {},
+        title: 'error'
+    });
+});
 
 
-
+module.exports = app;
